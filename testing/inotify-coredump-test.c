278fca544b9f2ea0343f8f54032e663003a59f0b
An Hoang <an.hoang@tu-braunschweig.de>
2024-05-03 12:48:50 +0200

    added test for inotify-coredump-test.c

---
+++ testing/inotify-coredump-test.c
@@ -0,0 +1,16 @@
+#include "MQTTClient.h"
+#include "bridge.h"
+#include "inotify-coredump.h"
+#include <fcntl.h> /* For O_* constants */
+#include <mqueue.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h> /* For mode constants */
+#include <unistd.h>
+
+int main()
+{
+    inotify_coredump();
+    return 0;
+}
