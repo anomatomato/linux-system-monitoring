d94ac7b797ec2ecaa06e5cd484d173a31271c6fe
Karen Meliksetian <karen.meliksetian@deep5.io>
2024-05-04 14:08:09 +0200

    feat: main.c threadig enabled

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include "bridge.h"
 #include "inotify-coredump.h"
-
+#include <pthread.h>
 int main()
 {
 
-    bridge();
-    // inotify_coredump();
+    pthread_t main_thread;
+    pthread_create(&main_thread, NULL, bridge, NULL);
+    pthread_t inotify_coredump_thread;
+    pthread_create(&inotify_coredump_thread, NULL, inotify_coredump, NULL);
     return 0;
 }
