adb4e53477d02d3f80f066510e8f5796be3e15b6
An Hoang <an.hoang@tu-braunschweig.de>
2024-05-04 13:38:00 +0200

    Added get_timestamp()

--- utilities/mq.h
+++ utilities/mq.h
@@ -1,4 +1,8 @@
 #define MQ_PATH "/my_queue"
 #define MAX_MSG_SIZE 150
 
+/* Send message in InfluxDB Line Protocol to posix-mq-to-mqtt-bridge */
 int send_to_mq(const char* message);
+
+/* Get timestamp in nanoseconds */
+long long get_timestamp();
