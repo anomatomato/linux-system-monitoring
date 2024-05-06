99a4b987119ad6687bb25a11093a0fb7edb9902f
ngoc2210 <phamngochochiminh2210@gmail.com>
2024-05-06 23:42:09 +0200

    revert to the previous state

--- inotify-pacct/get_pacct.c
+++ inotify-pacct/get_pacct.c
@@ -15,12 +15,10 @@
 // char* diff = "/opt/stats-recording/inotify-pacct/src/diff.txt";
 
 // local path
-const char* binary_pacct = "/var/log/pacct/binary_pacct";
-const char* original_pacct =
-    "/var/log/pacct/original_pacct.txt";
-const char* copy_pacct =
-    "/var/log/pacct/copy_pacct.txt";
-const char* diff = "/var/log/pacct/diff.txt";
+const char* binary_pacct   = "/var/log/pacct/binary_pacct";
+const char* original_pacct = "/var/log/pacct/original_pacct.txt";
+const char* copy_pacct     = "/var/log/pacct/copy_pacct.txt";
+const char* diff           = "/var/log/pacct/diff.txt";
 
 /**
  * Copies a file from the source path to the destination path.
@@ -107,13 +105,6 @@ void write_diff(const char* old_file, const char* new_file,
  */
 int enable_process_accounting()
 {
-    FILE* file = fopen(binary_pacct, "w");
-    if (!file)
-    {
-        fprintf(stderr, "Error opening file\n");
-        exit(1);
-    }
-   
     // Attempt to enable process accounting
     if (acct(binary_pacct) == -1)
     {
@@ -123,8 +114,8 @@ int enable_process_accounting()
 
     printf("Process accounting enabled. Records are being saved to: %s\n",
            binary_pacct);
-    
-    fclose(file);
+
+    // fclose(file);
 
     sleep(10);
 
@@ -145,7 +136,7 @@ int enable_process_accounting()
  * The function uses the `dump-acct` command to convert the binary accounting
  * data stored in `binary_acct` to a human-readable format and writes it to the
  * specified text file located at
- * `/opt/stats-recording/inotify-pacct/src/pacct.txt`.
+ * `/var/log/pacct.txt`.
  *
  * @return Returns `EXIT_SUCCESS` if the conversion and writing process is
  * successful, otherwise returns `EXIT_FAILURE`.
@@ -153,6 +144,14 @@ int enable_process_accounting()
 int convert_binary_to_text()
 {
     char command[256];
+    printf("alo");
+    FILE* file = fopen(original_pacct, "w");
+    if (!file)
+    {
+        fprintf(stderr, "Error opening file\n");
+        exit(1);
+    }
+    printf("alo");
     snprintf(command, sizeof(command), "dump-acct %s > %s", binary_pacct,
              original_pacct);
 
@@ -180,14 +179,14 @@ int convert_binary_to_text()
  * @return int Returns EXIT_SUCCESS if the process accounting is successful,
  * otherwise returns EXIT_FAILURE.
  */
-int get_pacct()
+int main()
 {
     if (enable_process_accounting() == EXIT_FAILURE)
     {
         fprintf(stderr, "Error in process accounting. Exiting.\n");
         return EXIT_FAILURE;
     }
-
+    printf("alo");
     if (convert_binary_to_text() == EXIT_FAILURE)
     {
         fprintf(stderr,
