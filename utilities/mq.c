b83144e0f9a0b8432570b6daaa9da2b570a27373
An Hoang <an.hoang@tu-braunschweig.de>
2024-05-13 18:05:21 +0200

    Added init_mq

--- utilities/mq.c
+++ utilities/mq.c
@@ -6,9 +6,29 @@
 #include <sys/stat.h> /* For mode constants */
 #include <time.h>
 
+int init_mq(const char* mq_path)
+{
+    mq_unlink(mq_path);
+
+    /* Set attributes for the message queue*/
+    struct mq_attr attr;
+    attr.mq_maxmsg  = MAX_MESSAGES;
+    attr.mq_msgsize = MAX_MSG_SIZE;
+
+    /* Create the message queue */
+    mqd_t mq = mq_open(mq_path, (O_CREAT | O_RDWR), (S_IRUSR | S_IWUSR), &attr);
+    if (mq == -1)
+    {
+        perror("mq_open failed");
+        return -1;
+    }
+
+    return mq;
+}
+
 int send_to_mq(const char* message, const char* mq_path)
 {
-    printf("send_to_mq: %s\n", message);
+    /* Open the message queue for writing */
     mqd_t mq = mq_open(mq_path, O_WRONLY);
     if (mq == -1)
     {
@@ -16,13 +36,13 @@ int send_to_mq(const char* message, const char* mq_path)
         return -1;
     }
 
+    /* Send message to bridge */
     if (mq_send(mq, message, strlen(message) + 1, 0) == -1)
     {
         perror("mq_send failed");
         mq_close(mq);
         return -1;
     }
-    mq_close(mq);
     return 0;
 }
 
