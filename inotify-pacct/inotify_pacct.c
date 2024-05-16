4f172c6c0743b054e5befca551d4fdf267bb04f7
ngoc2210 <phamngochochiminh2210@gmail.com>
2024-05-17 01:57:44 +0200

    buildable version, getting acct doens't run

--- inotify-pacct/inotify_pacct.c
+++ inotify-pacct/inotify_pacct.c
@@ -1,44 +1,32 @@
+#include "inotify_pacct.h"
 #include "../utilities/mq.h"
-#include "get_pacct.h"
 #include <errno.h>
 #include <fcntl.h>        /* For O_* constants */
 #include <linux/limits.h> /* For NAME_MAX */
 #include <mqueue.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/acct.h>
 #include <sys/inotify.h>
 #include <sys/stat.h> /* For mode constants */
 #include <unistd.h>
-#include "inotify_pacct.h"
 
-// server path
-// #define WATCH_FILE "/opt/stats-recording/inotify-pacct/src/diff.txt"
-
-#define WATCH_FILE "/var/log/pacct/diff.txt"
+#define BINARY "/var/log/pacct/binary"
+#define PACCT "/var/log/pacct/pacct"
 #define EVENT_SIZE (sizeof(struct inotify_event))
 #define BUF_LEN                                                                \
     (1024 *                                                                    \
      (EVENT_SIZE + NAME_MAX + 1)) /* enough for 1024 events in the buffer */
 
-/**
- * @file inotify_pacct.c
- * @brief This file contains the implementation of the inotify_pacct function.
- * 
- * The inotify_pacct function sets up an inotify instance, adds a watch for a specified accounting file,
- * and continuously reads events from the inotify file descriptor. When a modification event is detected,
- * the function opens the file, reads its content, and sends the content to message queue.
- * 
- * @return Returns EXIT_SUCCESS if the function executes successfully, otherwise returns EXIT_FAILURE.
- */
-int inotify_pacct()
+int main()
 {
     int fd, wd, len;
     char buffer[BUF_LEN];
     printf("inotify_pacct running...\n");
     // Create an inotify instance
     fd = inotify_init();
-    init_mq("/inotify_pacct");
+    // init_mq("/inotify_pacct");
     if (fd < 0)
     {
         perror("Failed to initialize inotify");
@@ -46,7 +34,7 @@ int inotify_pacct()
     }
 
     // Add a watch for the accounting file
-    wd = inotify_add_watch(fd, WATCH_FILE, IN_MODIFY);
+    wd = inotify_add_watch(fd, PACCT, IN_MODIFY);
     if (wd < 0)
     {
         perror("Failed to add inotify watch");
@@ -69,56 +57,45 @@ int inotify_pacct()
             struct inotify_event* event = (struct inotify_event*)&buffer[i];
             if (event->mask & IN_MODIFY)
             {
-                FILE* diff_file = fopen(WATCH_FILE, "r");
-                if (diff_file == NULL)
+                FILE* fp = fopen(PACCT, "r");
+                if (fp == NULL)
                 {
-                    perror("Failed to open diff.txt");
-                    return -1;
+                    perror("Failed to open file");
+                    return 1;
                 }
 
+                struct acct_v3 account;
 
-                // check if the file is empty
+                char message[MAX_MSG_SIZE];
 
-                //  moves the file position indicator to the end of the file
-                fseek(diff_file, 0, SEEK_END);
-                // returns the current value of the file position indicator
-                long filesize = ftell(diff_file);
-                // moves the file position indicator back to the beginning of
-                // the file
-                rewind(diff_file);
-                
-                if (filesize > 0)
+                while(fread(&account, sizeof(struct acct_v3), 1, fp) == 1)
                 {
-                    // The file is not empty, read its content
-                    char* message = malloc(filesize + 1);
-                    if (message == NULL)
-                    {
-                        perror("Failed to allocate memory for message");
-                        return -1;
-                    }
-
-                    fread(message, 1, filesize, diff_file);
-                    message[filesize] = '\0'; // Null-terminate the string
-
-                    printf("%s\n", message);
-
-                    if (send_to_mq(message, MESSAGE_QUEUES[1]) == -1)
-                    {
-                        perror("send_to_mq failed");
-                    }
-
-                    free(message);
+                    acc
                 }
-                else
-                {
-                    printf("The file is empty.\n");
-                }
-                fclose(diff_file);
+
+                snprintf(message, 1024,
+                         "pacct,raspi=pi-85cd "
+                         "ac_flag=%c,ac_version=%c,ac_tty=%u,ac_exitcode=%u,ac_"
+                         "uid=%u,"
+                         "ac_gid=%u,ac_pid=%u,ac_ppid=%u,ac_btime=%f,ac_etime=%d,ac_"
+                         "utime=%u,ac_stime=%u,ac_mem=%u,ac_io=%u,ac_rw=%u,ac_"
+                         "minflt=%u,ac_majflt=%u,ac_swaps=%u,ac_comm=\"%s\"",
+                         account.ac_flag, account.ac_version, account.ac_tty,
+                         account.ac_exitcode, account.ac_uid, account.ac_gid,
+                         account.ac_pid, account.ac_ppid, account.ac_btime,
+                         account.ac_etime, account.ac_utime, account.ac_stime,
+                         account.ac_mem, account.ac_io, account.ac_rw,
+                         account.ac_minflt, account.ac_majflt, account.ac_swaps,
+                         account.ac_comm);
+                printf("Message: %s\n", message);
+                // if (send_to_mq(message, MESSAGE_QUEUES[1]) == -1)
+                // {
+                //     perror("send_to_mq failed");
+                // }
             }
             i += EVENT_SIZE + event->len;
         }
     }
-
     // Cleanup
     inotify_rm_watch(fd, wd);
     close(fd);
--- inotify-pacct/inotify_pacct.h
+++ inotify-pacct/inotify_pacct.h
@@ -1,6 +1,35 @@
 #ifndef _GET_PACCT_H_
 #define _GET_PACCT_H_
 
+/**
+ * Converts binary accounting data to a human-readable text file.
+ * The function uses the `dump-acct` command to convert the binary accounting
+ * data stored in `binary_acct` to a human-readable format and writes it to the
+ * specified text file located at
+ * `/var/log/pacct`.
+ *
+ * @return Returns `EXIT_SUCCESS` if the conversion and writing process is
+ * successful, otherwise returns `EXIT_FAILURE`.
+ */
+int convert_binary_to_text();
+
+/**
+ * Enables process accounting, saves records to a specified file,
+ *
+ * @return EXIT_SUCCESS if process accounting is enabled and disabled
+ * successfully, EXIT_FAILURE otherwise.
+ */
+int enable_process_accounting();
+
+/**
+ * The inotify_pacct function sets up an inotify instance, adds a watch for a
+ * specified accounting file, and reads events from the inotify
+ * file descriptor. When a modification event is detected, the metrics from acct instance are gotten
+ * and sent to a message queue.
+ * 
+ * @return Returns EXIT_SUCCESS if the function executes successfully, otherwise
+ * returns EXIT_FAILURE.
+ */
 int inotify_pacct();
 
 #endif
