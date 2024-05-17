3f9275ff6a10787450c2649d47f59fdf0efb8cbd
Emre Onbasiogullari <e.onbasiogullari@tu-braunschweig.de>
2024-05-17 11:15:03 +0200

    cfr im cmake

--- cyclic-file-read/src/cycle-file-read.c
+++ cyclic-file-read/src/cycle-file-read.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "cyclic-file-read.h"
+#include <unistd.h>
 #include "stat.h"
 #include "net.h"
 #include "disk.h"
@@ -8,33 +8,36 @@
 #include "common.h"
 #include "../../utilities/mq.h"
 
-int cfr() {
+int main() {
         if (init_mq("/cfr") == -1)
                 return 1;
 
-        if (stat() == 1) {
-                perror("stat");
-                printf("Failed to read /proc/stat\n");
-        }
-        if (net() == 1) {
-                perror("net");
-                printf("Failed to read /proc/net/dev\n");
-        }
-        if (disk() == 1) {
-                perror("disk");
-                printf("Failed to read /proc/diskstats\n");
-        }
-        /*if (pid("stat") == 1) {
-                perror("pid");
-                printf("Failed to read /proc/<pid>/stat\n");
-        }
-        if (pid("statm") == 1) {
-                perror("pid");
-                printf("Failed to read /proc/<pid>/statm\n");
-        }*/
-        //sys();
+        while (1) {
+                if (stat() == 1) {
+                        perror("stat");
+                        printf("Failed to read /proc/stat\n");
+                }
+                if (net() == 1) {
+                        perror("net");
+                        printf("Failed to read /proc/net/dev\n");
+                }
+                if (disk() == 1) {
+                        perror("disk");
+                        printf("Failed to read /proc/diskstats\n");
+                }
+                /*if (pid("stat") == 1) {
+                        perror("pid");
+                        printf("Failed to read /proc/<pid>/stat\n");
+                }
+                if (pid("statm") == 1) {
+                        perror("pid");
+                        printf("Failed to read /proc/<pid>/statm\n");
+                }*/
+                //sys();
 
-        dequeue();
+                dequeue();
+                sleep(10);
+        }
 
         return 0;
 }
