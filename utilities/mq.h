ce8afcf1a9c2051af2657e99c9d1bab418ee4ecf
An Hoang <an.hoang@tu-braunschweig.de>
2024-06-06 16:24:41 +0200

    Minor fix

--- utilities/mq.h
+++ utilities/mq.h
@@ -17,6 +17,7 @@
 /**
  * @brief init_mq initializes a message queue with the given name and returns
  * the file descriptor or failure
+ *
  * @param mq_path: The identifying name of the message queue
  * @return File descriptor of the new message queue on success, -1 on failure
  * with errno set appropriately
@@ -28,6 +29,7 @@ int init_mq(const char* mq_path);
 /**
  * @brief send_to_mq sends the given message to the given message queue and
  * returns success or failure
+ *
  * @param message: The message to send
  * @param mq_path: The identifying name of the message queue
  * @return 0 on success, -1 on failure with errno set appropriately
@@ -38,6 +40,7 @@ int send_to_mq(const char* message, const char* mq_path);
 
 /**
  * @brief get_timestamp returns the current time in nanoseconds
+ *
  * @return The current time in nanoseconds
  */
 long long get_timestamp();
