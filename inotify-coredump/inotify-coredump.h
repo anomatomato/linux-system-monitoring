e62899204acef1fc6260e3180cdc0b9a1a043ce7
An Hoang <an.hoang@tu-braunschweig.de>
2024-06-10 22:17:18 +0200

    Added Comments to inotify-coredump

--- inotify-coredump/inotify-coredump.h
+++ inotify-coredump/inotify-coredump.h
@@ -3,6 +3,15 @@
 
 extern volatile int keep_running;
 
+/**
+ * @brief Signal handler function for gracefully shutting down the program.
+ *
+ * This function is set as the handler for SIGINT, SIGTERM, and other signals.
+ * When a signal is received, this function sets keep_running to 0, which indicates the program should
+ * stop running.
+ *
+ * @param signal The signal number of the received signal.
+ */
 extern void handle_signal(int signal);
 
 /** @brief Whenever a coredump is created, send a message to the message queue
