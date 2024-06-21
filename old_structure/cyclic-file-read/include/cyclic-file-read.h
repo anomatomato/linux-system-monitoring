099b1cce109fad21d00ddfc1d51ba9c124e7da26
An Hoang <an.hoang@tu-braunschweig.de>
2024-06-21 23:29:02 +0200

    Added old structure

---
+++ old_structure/cyclic-file-read/include/cyclic-file-read.h
@@ -0,0 +1,8 @@
+#ifndef _CFR_H_
+#define _CFR_H_
+
+#include <stdio.h>
+
+int cfr();
+
+#endif
---
+++ old_structure/cyclic-file-read/include/net.h
@@ -0,0 +1,8 @@
+#ifndef _CFR_NET_H_
+#define _CFR_NET_H_
+
+#define NET_FILE "/proc/net/dev"
+
+int net();
+
+#endif
