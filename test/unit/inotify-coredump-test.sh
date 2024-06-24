d9839e643d6b5ef9bf54ac30e99ce86eab956b59
An Hoang <an.hoang@tu-braunschweig.de>
2024-06-24 20:19:01 +0200

    sleeps

--- test/unit/inotify-coredump-test.sh
+++ test/unit/inotify-coredump-test.sh
@@ -5,7 +5,7 @@ printf "Starting Unit test for inotify-coredump...\n\n"
 # Start inotify-coredump in test mode
 ./../../build/inotify-coredump-exec -t -v &
 INOTIFY_PID=$!
-sleep 1
+sleep 4
 ps $INOTIFY_PID
 
 # Create coredump file
@@ -14,29 +14,8 @@ echo "Generating coredump..."
 sleep 500 &
 kill -s SIGTRAP $(pgrep sleep)
 
-# Create a simple C program that causes a segmentation fault
-cat <<EOF >segmentation_fault.c
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    int *p = NULL;
-    *p = 42;  // This line causes a segmentation fault
-    return 0;
-}
-EOF
-
-# Compile the C program
-gcc -o segmentation_fault segmentation_fault.c
-
-# Run the segmentation fault program to generate a coredump
-echo "Creating segmentation fault to generate coredump..."
-set +e
-./segmentation_fault
-set -e
-
 ps $INOTIFY_PID
-
+sleep 2
 # Wait for inotify-coredump to finish and capture the exit status
 wait $INOTIFY_PID
 EXIT_STATUS=$?
