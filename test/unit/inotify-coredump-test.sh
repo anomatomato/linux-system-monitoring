de1da245e0e87630318844c9d6e212c1f4b0c0c3
An Hoang <an.hoang@tu-braunschweig.de>
2024-06-24 04:47:18 +0200

    Fixed inotify-coredump test

--- test/unit/inotify-coredump-test.sh
+++ test/unit/inotify-coredump-test.sh
@@ -3,7 +3,7 @@
 ./../../build/inotify-coredump-exec -t &
 sleep 1
 
-cd ../create_coredump
+cd ../create_coredumps
 gcc -o segmentation_fault segmentation_fault.c
 ./segmentation_fault
 killall inotify-coredump-exec
