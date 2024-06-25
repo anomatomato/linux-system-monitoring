2868ec3e381a4124051bfc08d2ac7aebbeda5f17
An Hoang <an.hoang@tu-braunschweig.de>
2024-06-26 00:26:43 +0200

    New

--- src/inotify-coredump/inotify-coredump.c
+++ src/inotify-coredump/inotify-coredump.c
@@ -2,6 +2,7 @@
 #include "colored_output.h"
 #include "line_protocol_parser.h"
 #include "mq.h"
+#include "signal_handling.h"
 #include <errno.h>
 #include <fcntl.h>        /* For O_* constants */
 #include <linux/limits.h> /* For NAME_MAX */
@@ -18,10 +19,9 @@
 #define BUF_LEN (1024 * (EVENT_SIZE + NAME_MAX + 1)) /* enough for 1024 events in the buffer */
 
 
-extern volatile sig_atomic_t keep_running;
-
 /* Helper functions */
 /*------------------------------------------------------------------------------------------------------*/
+
 int init_inotify(int *fd, int *wd) {
         /* Initialize inotify instance */
         *fd = inotify_init();
@@ -47,7 +47,37 @@ int init_inotify(int *fd, int *wd) {
         return 0;
 }
 
-int process_events(int fd, int test, int verbose) {
+int coredump_to_line_protocol(char *buffer, const char *coredump_name) {
+        return snprintf(buffer,
+                        MAX_MSG_SIZE,
+                        "coredump,path=%s corefile=\"%s\" %lld",
+                        WATCH_DIR,
+                        coredump_name,
+                        get_timestamp());
+}
+
+int process_events(char *buffer, int len, int verbose) {
+        for (int i = 0; i < len;) {
+                struct inotify_event *event = (struct inotify_event *) &buffer[i];
+                if (event->len && event->mask & IN_CREATE) {
+                        char message[MAX_MSG_SIZE];
+                        coredump_to_line_protocol(message, event->name);
+
+                        if (verbose)
+                                printf("Message:\n%s\n", message);
+
+                        /* Send message to message queue */
+                        if (send_to_mq(message, MQ_PATH) == -1) {
+                                perror("send_to_mq failed");
+                                return -1;
+                        }
+                }
+                i += EVENT_SIZE + event->len;
+        }
+        return 0;
+}
+
+int event_loop(int fd, int test, int verbose) {
         char buffer[BUF_LEN];
         /* Event loop */
         while (keep_running) {
@@ -59,47 +89,9 @@ int process_events(int fd, int test, int verbose) {
                         perror("read failed");
                         return -1;
                 }
-
                 /* Process events */
-                for (int i = 0; i < len;) {
-                        struct inotify_event *event = (struct inotify_event *) &buffer[i];
-                        if (event->len && event->mask & IN_CREATE) {
-                                char message[MAX_MSG_SIZE];
-                                snprintf(message,
-                                         MAX_MSG_SIZE,
-                                         "coredump,path=%s corefile=\"%s\" %lld",
-                                         WATCH_DIR,
-                                         event->name,
-                                         get_timestamp());
-
-                                if (verbose)
-                                        printf("Message:\n%s\n", message);
-
-                                /* Send message to message queue */
-                                if (send_to_mq(message, MQ_PATH) == -1) {
-                                        perror("send_to_mq failed");
-                                        return -1;
-                                }
-
-                                if (test) {
-                                        int status = LP_check(message);
-                                        if (status != 0) {
-                                                fprintf(stderr,
-                                                        COLOR_RED STYLE_BOLD
-                                                        "Message is not in InfluxDB Line Protocol!" RESET_ALL
-                                                        "\n");
-                                                return -1;
-                                        }
-                                        printf(COLOR_GREEN STYLE_BOLD
-                                               "Message is in InfluxDB Line Protocol" RESET_ALL "\n");
-                                        printf("Raising SIGTERM...\n");
-                                        raise(SIGTERM);
-                                }
-                        }
-                        i += EVENT_SIZE + event->len;
-                }
+                int pe = process_events(buffer, len, verbose);
         }
-
         return 0;
 }
 
@@ -130,7 +122,7 @@ int inotify_coredump(int test, int verbose) {
                 printf(COLOR_GREEN STYLE_BOLD "inotify_coredump successfully initialized" RESET_ALL "\n");
 
 
-        int ret1 = process_events(fd, test, verbose);
+        int ret1 = event_loop(fd, test, verbose);
         int ret2 = cleanup(fd, wd);
         if (test)
                 printf(STYLE_BOLD "%s" RESET_ALL "\n",
