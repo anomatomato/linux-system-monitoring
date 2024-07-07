d973f39fb691062f42603171244a64aa7bf98ba4
Karen Meliksetian <karen.meliksetian@deep5.io>
2024-07-07 15:33:34 +0200

    added manpage and unit service for cyclic-sysinfo. added cyclic-sysinfo in installation config

--- cmake/Installation.cmake
+++ cmake/Installation.cmake
@@ -4,6 +4,9 @@ install(FILES
     ${CMAKE_BINARY_DIR}/cyclic-file-read-exec
     ${CMAKE_BINARY_DIR}/inotify-coredump-exec
     ${CMAKE_BINARY_DIR}/inotify-pacct-exec
+    ${CMAKE_BINARY_DIR}/epoll-psi-exec
+    ${CMAKE_BINARY_DIR}/dbus-systemd-signals-exec
+    ${CMAKE_BINARY_DIR}/cyclic-sysinfo-exec
     DESTINATION /usr/local/bin
     PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
 )
@@ -16,6 +19,7 @@ install(FILES
     systemd-services/inotify-pacct.service
     systemd-services/epoll-psi.service
     systemd-services/dbus-systemd-signals.service
+    systemd-services/cyclic-sysinfo.service
     DESTINATION /etc/systemd/system
     PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
 )
@@ -43,6 +47,7 @@ install(FILES
     manpages/inotify-pacct.8
     manpages/epoll-psi.8
     manpages/dbus-systemd-signals.8
+    manpages/cyclic-sysinfo.8
     DESTINATION /usr/local/share/man/man8
     PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
 )
