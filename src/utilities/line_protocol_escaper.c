a1ce3a9a51c8c938f73613a8f73bb577ddabd50f
An Hoang <an.hoang@tu-braunschweig.de>
2024-07-08 20:55:40 +0200

    line_protocol: added escape functions for special characters

---
+++ src/utilities/line_protocol_escaper.c
@@ -0,0 +1,35 @@
+#include "line_protocol_escaper.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+size_t escape_tag_value(const char *tag_value, char *output, size_t output_size) {
+        if (tag_value == NULL || output == NULL || output_size == 0)
+                return -1;
+
+        size_t i, j;
+        for (i = 0, j = 0; tag_value[i] != '\0', j < output_size - 1; i++) {
+                if (tag_value[i] == ' ' || tag_value[i] == '=' || tag_value == ',')
+                        output[j++] = '\\';
+
+                output[j++] = tag_value[i];
+        }
+        output[j] = '\0';
+        return j;
+}
+
+size_t escape_field_value(const char *tag_value, char *output, size_t output_size) {
+        if (tag_value == NULL || output == NULL || output_size == 0)
+                return -1;
+
+        size_t i, j;
+        for (i = 0, j = 0; tag_value[i] != '\0', j < output_size - 1; i++) {
+                if (tag_value[i] == '\\' || tag_value[i] == '"')
+                        output[j++] = '\\';
+
+                output[j++] = tag_value[i];
+        }
+        output[j] = '\0';
+        return j;
+}
---
+++ src/utilities/line_protocol_escaper.h
@@ -0,0 +1,5 @@
+#include <stddef.h>
+
+
+size_t escape_tag_value(const char *tag_value, char *output, size_t output_size);
+size_t escape_field_value(const char *tag_value, char *output, size_t output_size);
