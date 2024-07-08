16c4c3753b40633cecc23f3749a447ec81f80384
An Hoang <an.hoang@tu-braunschweig.de>
2024-07-08 02:18:28 +0200

    Dockerfile: removed redundant packages, added libdbus

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,23 @@ RUN apt-get update \
     && apt-get install -y \
     crossbuild-essential-arm64 \
     crossbuild-essential-armhf \
-    debhelper \
     dpkg-dev \
     cmake \
+    make \
     libsensors4-dev \
+    libdbus-1-dev \
+    libdbus-1-3 \
     systemd-coredump \
     git \
     acct \
     sudo \
     && apt-get clean && rm -rf /var/lib/lists/*
+
+WORKDIR /app
+
+COPY . .
+
+CMD ["./build.sh"]
 
 # Clone and install paho.mqtt.c
 # RUN git clone https://github.com/eclipse/paho.mqtt.c.git && \
--- cmake/Packaging.cmake
+++ cmake/Packaging.cmake
@@ -8,8 +8,8 @@ set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Your Name <your.email@example.com>")
 
 # Highly Recommended
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "C application for sending statistics to Mosquitto")
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "debhelper (>= 11)")
-set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "any") # Adjust
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libdbus-1-dev, systemd-coredump, libsensors4-dev, acct")
+set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "${DEB_ARCH}") # Adjust
 
 # Good Practice
 set(CPACK_DEBIAN_FILE_NAME "DEB-DEFAULT")
@@ -18,6 +18,7 @@ set(CPACK_DEBIAN_PACKAGE_SECTION "base")
 set(CPACK_DEBIAN_PACKAGE_PRIORITY "optional")
 
 # Additional
+set(CPACK_OUTPUT_FILE_PREFIX "${CMAKE_SOURCE_DIR}/debs")
 set(CPACK_DEBIAN_PACKAGE_HOMEPAGE "https://gitlab.ibr.cs.tu-bs.de/cm-courses/sep/groups/ss24/cm0/stats-recording")
 set(CPACK_DEBIAN_PACKAGE_RELEASE "1")
 
