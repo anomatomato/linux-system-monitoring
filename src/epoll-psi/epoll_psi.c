b357beabad18974bc8007b46edd64ed73bf8b661
Karen Meliksetian <karenjantv@gmail.com>
2024-07-09 22:28:13 +0200

    epoll psi fix

--- src/epoll-psi/epoll_psi.c
+++ src/epoll-psi/epoll_psi.c
@@ -106,8 +106,6 @@ int *register_files_in_dir(int *fds, struct epoll_event *event, char *dir_name, 
                 char path[256];
                 strcpy(path, dir_name);
                 strcat(path, resources[i]);
-                if (strstr("/sys/fs/cgroup", dir_name) != NULL)
-                        strcat(path, ".pressure");
                 // snprintf(path, sizeof(path), "/proc/pressure/%s", resources[i]);
                 printf("path:%s\n, dir:%s", path, dir_name);
                 fds[i] = open(path, O_RDONLY | O_NONBLOCK);
