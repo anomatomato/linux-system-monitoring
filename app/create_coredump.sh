f5f0033d309611299cd4b08db0d81a074c5e8fd4
An Hoang <an.hoang@tu-braunschweig.de>
2024-07-09 21:23:56 +0200

    demo-app: create coredump check

--- app/create_coredump.sh
+++ app/create_coredump.sh
@@ -5,4 +5,4 @@ echo "Creating coredump..."
 sleep 50 &
 kill -s SIGTRAP $(pgrep sleep)
 
-coredumpctl info --no-pager
+coredumpctl info --no-pager || echo "Required: sudo apt-get install systemd-coredump"
