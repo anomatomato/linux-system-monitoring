d1c4c11f68005f5ba8818c83ebe4f9f7f202c63d
Emre Onbasiogullari <e.onbasiogullari@tu-braunschweig.de>
2024-07-09 21:26:31 +0200

    app update

--- app/main.c
+++ app/main.c
@@ -1,9 +1,10 @@
-#include "bridge.h"
-#include "inotify-coredump.h"
 #include <pthread.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 
 void fill_memory(){
@@ -14,11 +15,61 @@ void fill_memory(){
                 array[i] = memory;
                 i++;
         }
-        for (int i = 0; i++; i < 100) {
+        for (int i = 0; i < 100; i++) {
                 free(array[i]);
         }
 }
 
+void calc_pi() {
+        long int n = 1000000000, i;
+        double sum=0.0, term, pi;
+
+        /* Applying Leibniz Formula */
+        for (i=0; i < n; i++) {
+                term = pow(-1, i) / (2*i+1);
+                sum += term;
+        }
+        pi = 4 * sum;
+        printf("%f\n", pi);
+}
+
 int main() {
-        fill_memory();
+        int j;
+        //while (j < 20) {
+                //pid_t p = fork();
+                //if(p < 0) {
+                        //perror("fork fail");
+                        //break;
+                //}
+                //else if (p == 0) {
+                        //printf("Hello from Child!\n");
+                        //fill_memory();
+                        //continue;
+                //}
+                //else {
+                        //printf("Hello from Parent!\n");
+                        //calc_pi();
+                        //wait();
+                        //break;
+                //}
+        //}
+
+        for (j = 0; j < 20; j++) {
+                pid_t p = fork();
+                if(p < 0) {
+                        perror("fork fail");
+                        break;
+                }
+                else if (p == 0) {
+                        printf("Hello from Child!\n");
+                        fill_memory();
+                        _exit(0);
+                }
+                else {
+                        printf("Hello from Parent!\n");
+                        calc_pi();
+                        wait(NULL);
+                        break;
+                }
+        }
 }
